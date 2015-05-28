56d965d2f1d83fc982ea92e0b90a45bcf2e578c9
Tamas Kenez <tamas.kenez@kishonti.net>
2015-05-28 11:57:16 +0200

    removed obsolete files, cmds moved to bin

--- cake-depends-db.cmake
+++
@@ -1,5 +0,0 @@
-set(CAKE_PKG_URL_OF_ZLIB https://github.com/madler/zlib.git)
-set(CAKE_PKG_URL_OF_PNG git://git.code.sf.net/p/libpng/code)
-cake_pkg_depends(NAME PNG CODE "cake_pkg(INSTALL NAME ZLIB)")
-
-
